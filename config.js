861ca510359c0f543b92810a3cb46ba644c0cd62
Michael Meminger <mmeminger1@gmail.com>
2021-04-21 15:54:41 -0400

    Initial commit

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'secretKey': "12345-67890-09876-54321",
+  'mongoUrl': "mongodb://localhost:27017/nucampsite",
+  'facebook': {
+    clientId: '239897797577845',
+    clientSecret: '89ea9f53de05b95009f513c2d74dff60'
+  },
+};
+
+//Install this package in the project folder. npm install passport-jwt@4.0.0 jsonwebtoken@8.5.1 
+//Then create this file config.js and export the following above. 
